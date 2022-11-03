ca0e3a7f77c5d35a723085e0c51d7583b1efe55c
pederan <peder1976@gmail.com>
2022-11-03 13:19:04 +0100

    Removed pointer events to admin bar item, re-ordered hooks according to filter order

--- plugin.php
+++ plugin.php
@@ -18,17 +18,44 @@ namespace Dekode\Label_Environment;
 if ( \apply_filters( 'dekode_label_environment_enabled', true ) ) {
 
 	\add_action( 'admin_bar_menu', __NAMESPACE__ . '\\add_environment_to_admin_bar', 999 );
+	\add_action( 'admin_head', __NAMESPACE__ . '\\add_admin_bar_styles', 999 );
 
-	// Display the banner on all environments except production.
-	if ( \apply_filters( 'dekode_label_environment_banner_enabled', ( 'production' !== \wp_get_environment_type() ) ) ) {
-		\add_action( 'wp_head', __NAMESPACE__ . '\\add_banner' );
-	}
 	/**
-	 * Add inline style to head for displaying the banner
+	 * Add information about the environment in the admin bar
+	 *
+	 * @param \WP_Admin_Bar $wp_admin_bar The WP_Admin_Bar instance, passed by reference.
 	 *
 	 * @return void
 	 */
-	function add_banner() {
+	function add_environment_to_admin_bar( \WP_Admin_Bar $wp_admin_bar ) {
+		$args = [
+			'id'    => 'dekode-label-environment',
+			'title' => \sprintf( 'Environment: %s', \esc_html( \wp_get_environment_type() ) ),
+		];
+		$wp_admin_bar->add_node( $args );
+	}
+
+	// Display the banner on all environments except production.
+	if ( \apply_filters( 'dekode_label_environment_banner_enabled', ( 'production' !== \wp_get_environment_type() ) ) ) {
+		\add_action( 'wp_head', __NAMESPACE__ . '\\add_banner_styles' );
+	}
+
+	/**
+	 * Add inline style to head for displaying admin bar environment information in admin
+	 * Remove pointer events since the text is not linked
+	 *
+	 * @return void
+	 */
+	function add_admin_bar_styles() {
+		echo '<style>#wp-admin-bar-dekode-label-environment{pointer-events:none;}</style>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
+
+	/**
+	 * Add inline style to head for displaying the banner on frontend
+	 *
+	 * @return void
+	 */
+	function add_banner_styles() {
 		$css = 'body:after{'
 		. 'align-items:center;'
 		. 'background-color:#000;'
@@ -55,20 +82,5 @@ if ( \apply_filters( 'dekode_label_environment_enabled', true ) ) {
 		. '}'
 		. '}';
 		echo '<style>' . $css . '</style>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	}
-
-	/**
-	 * Add information about the environment in the admin bar
-	 *
-	 * @param \WP_Admin_Bar $wp_admin_bar The WP_Admin_Bar instance, passed by reference.
-	 *
-	 * @return void
-	 */
-	function add_environment_to_admin_bar( \WP_Admin_Bar $wp_admin_bar ) {
-		$args = [
-			'id'    => 'dekode-label-environment',
-			'title' => \sprintf( 'Environment: %s', \esc_html( \wp_get_environment_type() ) ),
-		];
-		$wp_admin_bar->add_node( $args );
 	}
 }
